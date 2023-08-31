bd7181826e0072d38b634e840f0b2712ba727972
JAEWUNG LEE <james@jamesnet.dev>
2023-08-31 23:21:49 +0900

    Updates

--- src/BlurSharp/BlurSharp.Support/UI/Units/DarkWindow.cs
+++ src/BlurSharp/BlurSharp.Support/UI/Units/DarkWindow.cs
@@ -8,30 +8,41 @@ namespace BlurSharp.Support.UI.Units
 {
     public class DarkWindow : JamesWindow
     {
-        // Using a DependencyProperty as the backing store for TitleContent.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TitleTemplateProperty = DependencyProperty.Register("TitleTemplate", typeof(DataTemplate), typeof(DarkWindow), new PropertyMetadata(null));
+        public static readonly DependencyProperty CloseCommandProperty = 
+            DependencyProperty.Register("CloseCommand", typeof(ICommand), typeof(DarkWindow), new PropertyMetadata(null));
+        public static readonly DependencyProperty TitleTemplateProperty = 
+            DependencyProperty.Register("TitleTemplate", typeof(DataTemplate), typeof(DarkWindow), new PropertyMetadata(null));
+        public static readonly new DependencyProperty TitleProperty = 
+            DependencyProperty.Register("Title", typeof(object), typeof(DarkWindow), new UIPropertyMetadata(null));
 
-        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(object), typeof(DarkWindow), new UIPropertyMetadata(null));
         public DataTemplate TitleTemplate
         {
             get { return (DataTemplate)GetValue(TitleTemplateProperty); }
             set { SetValue(TitleTemplateProperty, value); }
         }
+
         public new object Title
         {
             get => GetValue(TitleTemplateProperty);
             set => SetValue(TitleTemplateProperty, value);
         }
 
+        public ICommand CloseCommand
+        {
+            get => (ICommand)GetValue(CloseCommandProperty);
+            set => SetValue(CloseCommandProperty, value);
+        }
+
         static DarkWindow()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), new FrameworkPropertyMetadata(typeof(DarkWindow)));
         }
+
         public override void OnApplyTemplate()
         {
             if (GetTemplateChild("PART_CloseButton") is Button btn)
             {
-                btn.Click += (s, e) => Close();
+                btn.Click += (s, e) => WindowClose();
             }
             if (GetTemplateChild("PART_MinButton") is Button minbtn)
             {
@@ -54,6 +65,18 @@ namespace BlurSharp.Support.UI.Units
             if (GetTemplateChild("PART_DragBar") is DraggableBar bar)
             {
                 bar.MouseDown += WindowDragMove;
+            }
+        }
+
+        private void WindowClose()
+        {
+            if (CloseCommand == null)
+            {
+                Close();
+            }
+            else
+            {
+                CloseCommand.Execute(this);
             }
         }
 
