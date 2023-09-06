63621870483dd416777c94d3d588b4028644ec29
lukewire <lukewire129@gmail.com>
2023-09-07 00:07:02 +0900

    - 유닛컨트롤 추가(SedTextBox, FileListBox

---
+++ src/BlurSharp/BlurSharp.Project/UI/Units/FileListBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BlurSharp.Project.UI.Units;
+
+public class FileListBox : ListBox
+{
+    static FileListBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(FileListBox), new FrameworkPropertyMetadata(typeof(FileListBox)));;
+    }
+}
