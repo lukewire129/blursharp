a28079c5e0655686b9ceeab86e160f05842b5721
lukewire <lukewire129@gmail.com>
2023-09-30 23:51:02 +0900

    RuleListbox 추가

---
+++ src/BlurSharp/BlurSharp.Project/UI/Units/RuleListBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BlurSharp.Project.UI.Units
+{
+    public class RuleListBox : ListBox
+    {
+        static RuleListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata (typeof (RuleListBox), new FrameworkPropertyMetadata (typeof (RuleListBox)));
+        }
+    }
+}
